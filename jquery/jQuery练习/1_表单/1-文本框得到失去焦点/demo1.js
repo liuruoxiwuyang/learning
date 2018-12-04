2242fe2fea72bd91ec4973db1ead8e384b64a941
若曦 <474384980@qq.com>
2018-12-04 16:56:06 +0800

    prop（特性）可以获取元素自带的属性，比如checked，id(特性的东西)

--- jquery/jQuery练习/1_表单/1-文本框得到失去焦点/demo1.js
+++
@@ -1,8 +0,0 @@
-    $(function(){
-		$(":input").focus(function(){//$("input, textarea")
-			  $(this).addClass("focus");
-			  //this.className = "focus";
-		}).blur(function(){
-			  $(this).removeClass("focus");
-		});
-    })
